20d53479cf64ef00827a1133a41756b63a6207aa
JoanArturo <joanarturo81@gmail.com>
2024-03-25 18:03:06 -0600

    feat: add date translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -12,6 +12,10 @@ import { ImageFormComponent } from './components/image-form/image-form.component
 import { ImageComponent } from './components/image/image.component';
 import { ImageModalComponent } from './components/image-modal/image-modal.component';
 import { ToastComponent } from './components/toast/toast.component';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -30,7 +34,12 @@ import { ToastComponent } from './components/toast/toast.component';
     ReactiveFormsModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
